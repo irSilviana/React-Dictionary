1d1eb313634e8d646b04034edb0fff623e8ba229
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-10 08:30:49 +0700

    Add dictionary API integration

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [data, setData] = useState(null);
 
   function handleKeyword(e) {
-    setKeyword(e.target.value);
+    setKeyword(e.target.value.trim());
+  }
+
+  function handleResponse(response) {
+    setData(response.data[0]);
+  }
+
+  function handleError(error) {
+    console.clear(error);
+    alert(
+      `😮 Sorry, we can't find the definition of "${keyword}" 
+Please type the correct word in English (US) 🌎`
+    );
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    alert(`Searching for the definition of "${keyword}"`);
+    alert(`🔎 Searching for the definition of "${keyword}"`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse).catch(handleError);
   }
 
   return (
