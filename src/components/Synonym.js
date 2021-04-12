d75b33f75b3d1815ce7e98f32cf02e8c24a53086
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-12 09:17:54 +0700

    Update Synonym.js
    return list in the component instead of span

--- src/components/Synonym.js
+++ src/components/Synonym.js
@@ -1,18 +1,15 @@
 import React from "react";
-import SynonymList from "./SynonymList";
 
 export default function Synonym(props) {
   if (props.synonym) {
     return (
       <div className="Synonym">
         Synonyms:
-        {props.synonym.map(function (synonym, index) {
-          return (
-            <span key={index}>
-              <SynonymList list={synonym} />
-            </span>
-          );
-        })}
+        <ul>
+          {props.synonym.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
       </div>
     );
   } else {
--- src/components/SynonymList.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SynonymList(props) {
-  return <span> {props.list},</span>;
-}
