6b52e7353be72a0a57745a69c88e9af63fdf319e
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-12 12:11:28 +0700

    Add Phonetic component with text & audio

---
+++ src/components/Phonetic.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+
+  let audio = new Audio(props.phonetic.audio);
+
+  function start() {
+    audio.play();
+  }
+
+  return (
+    <div className="Phonetic">
+      <FontAwesomeIcon icon={faPlayCircle} onClick={start} />{" "}
+      <span>{props.phonetic.text}</span>
+    </div>
+  );
+}
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function Result(props) {
-  console.log(props.result);
   if (props.result) {
     return (
       <div className="Result">
         <h2>{props.result.word}</h2>
-        <p>{props.result.phonetics[0].text}</p>
+
+        {props.result.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
 
         {/* Looping Meaning component
         <Meaning meaning={props.result.meanings[0]} />
