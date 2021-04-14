3cda8e13e482064aae4879531d5fb9507d15e63a
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-14 14:48:07 +0700

    Update Photos.js
    Delete console.log()

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  console.log(props.photos);
   if (props.photos.length > 0) {
     return (
       <section className="Photos">
